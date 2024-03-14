647ca10b8a05b6c77472d3741a57ed57246ca2d0
TahulelaN <145351126+TahulelaN@users.noreply.github.com>
2024-03-14 15:04:01 +0200

    Push SAWS 294

--- src/app/aero-sport/aero-sport.page.ts
+++ src/app/aero-sport/aero-sport.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit,ElementRef,HostListener  } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
@@ -9,8 +9,18 @@ import { AuthService } from '../services/auth.service';
 })
 export class AeroSportPage implements OnInit {
   isLogged: boolean = false;
+  isFormVisible: boolean = false;
+  isFormVisible1: boolean = false;
+  isFormVisible2: boolean = false;
+  isDropdownOpen1: boolean = false;
+  isDropdownOpen2: boolean = false;
+  isDropdownOpen3: boolean = false;
+  selectedOption1: string = 'Wind';
+  selectedOption2: string = 'Surface';
+  selectedOption3: string = 'Temperature';
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router, 
+    private authService: AuthService, private elRef: ElementRef) {}
 
   ngOnInit() {
     // Check if user is logged in
@@ -20,12 +30,65 @@ export class AeroSportPage implements OnInit {
    }
 
  }
+ @HostListener('document:click', ['$event'])
+ onClick(event: MouseEvent) {
+   if (!this.elRef.nativeElement.contains(event.target)) {
+     this.closeAllDropdowns();
+   }
+ }
+ toggleDropdown(dropdown: string) {
+  if (dropdown === 'dropdown1') {
+    this.isDropdownOpen1 = !this.isDropdownOpen1;
+    this.isDropdownOpen2 = false;
+    this.isDropdownOpen3 = false;
+  }
+
+   if (dropdown === 'dropdown2') {
+    this.isDropdownOpen2 = !this.isDropdownOpen2;
+    this.isDropdownOpen1 = false;
+    this.isDropdownOpen3 = false;
+  }
+  if (dropdown === 'dropdown3') {
+    this.isDropdownOpen3 = !this.isDropdownOpen3;
+    this.isDropdownOpen1 = false;
+    this.isDropdownOpen2 = false;
+  }
+}
+selectOption(option: string, dropdown: string) {
+  if (dropdown === 'dropdown1') {
+    this.selectedOption1 = option;
+    this.isDropdownOpen1 = false;
+  } else if (dropdown === 'dropdown2') {
+    this.selectedOption2 = option;
+    this.isDropdownOpen2 = false;
+  }
+}
+ closeAllDropdowns() {
+  this.isDropdownOpen1 = false;
+  this.isDropdownOpen2 = false;
+}
+
  get isLoggedIn(): boolean {
    return this.authService.getIsLoggedIn();
  }
 
+ toggleFormVisibility() {
+  this.isFormVisible = true;
+}
+toggleFormVisibility1() {
+  this.isFormVisible = true;
+}
+toggleFormVisibility2() {
+  this.isFormVisible = true;
+}
+
 
   aerosportPage() {
+    this.router.navigate(['/landing-page']);
+  }
+
+  
+  forecastPage() {
     this.router.navigate(['/landing-page']);
   }
 
--- src/app/forecast/forecast.page.ts
+++ src/app/forecast/forecast.page.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit,ElementRef,HostListener  } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 
 @Component({
   selector: 'app-forecast',
@@ -10,42 +11,70 @@ import { AuthService } from '../services/auth.service';
 export class ForecastPage implements OnInit {
   isLogged: boolean = false;
   isFormVisible: boolean = false;
-  // dropdownVisible: { [key: string]: boolean } = {
-  //   dropdown: false,
-  //   wind: false,
-  //   temperature: false
-  // };
+  isDropdownOpen1: boolean = false;
+  isDropdownOpen2: boolean = false;
+  isDropdownOpen3: boolean = false;
+  selectedOption1: string = 'Wind';
+  selectedOption2: string = 'Surface';
+  selectedOption3: string = 'Temperature';
   constructor(private router: Router,
-     private authService: AuthService,private elRef: ElementRef) {}
+     private authService: AuthService,
+     private elRef: ElementRef,private iab: InAppBrowser,) {}
 
   ngOnInit() {}
   get isLoggedIn(): boolean {
     return this.authService.getIsLoggedIn();
   }
-  @HostListener('document:click', ['$event'])
-  onClick(event: MouseEvent) {
-    if (!this.elRef.nativeElement.contains(event.target)) {
-      // this.isDropdownOpen = false;
-    }
-  }
-
-  // toggleDropdown(dropdownName: string) {
-  //   // Close all dropdowns
-  //   for (let key in this.dropdownVisible) {
-  //     if (key !== dropdownName) {
-  //       this.dropdownVisible[key] = false;
-  //     }
-  //   }
-
-
-  //   this.dropdownVisible[dropdownName] = !this.dropdownVisible[dropdownName];
-  // }
-
-
-
   toggleFormVisibility() {
     this.isFormVisible = !this.isFormVisible;
   }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    if (!this.elRef.nativeElement.contains(event.target)) {
+      this.closeAllDropdowns();
+    }
+  }
+
+
+  toggleDropdown(dropdown: string) {
+    if (dropdown === 'dropdown1') {
+      this.isDropdownOpen1 = !this.isDropdownOpen1;
+      this.isDropdownOpen2 = false;
+      this.isDropdownOpen3 = false;
+    }
+
+     if (dropdown === 'dropdown2') {
+      this.isDropdownOpen2 = !this.isDropdownOpen2;
+      this.isDropdownOpen1 = false;
+      this.isDropdownOpen3 = false;
+    }
+    if (dropdown === 'dropdown3') {
+      this.isDropdownOpen3 = !this.isDropdownOpen3;
+      this.isDropdownOpen1 = false;
+      this.isDropdownOpen3 = false;
+    }
+  }
+
+  selectOption(option: string, dropdown: string) {
+    if (dropdown === 'dropdown1') {
+      this.selectedOption1 = option;
+      this.isDropdownOpen1 = false;
+    } else if (dropdown === 'dropdown2') {
+      this.selectedOption2 = option;
+      this.isDropdownOpen2 = false;
+    }
+  }
+   closeAllDropdowns() {
+    this.isDropdownOpen1 = false;
+    this.isDropdownOpen2 = false;
+  }
+
+
+
+
+
+  
   forecastPage() {
     this.router.navigate(['/landing-page']);
   }
