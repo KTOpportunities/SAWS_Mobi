66285ff6c9a289fdddf52b5eead0a08925cfa56d
TahulelaN <145351126+TahulelaN@users.noreply.github.com>
2024-03-14 08:40:59 +0200

    SAWS 281

--- src/app/forecast/forecast.page.ts
+++ src/app/forecast/forecast.page.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit,ElementRef,HostListener  } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
@@ -9,13 +9,45 @@ import { AuthService } from '../services/auth.service';
 })
 export class ForecastPage implements OnInit {
   isLogged: boolean = false;
-  constructor(private router: Router, private authService: AuthService) {}
+  isFormVisible: boolean = false;
+  // dropdownVisible: { [key: string]: boolean } = {
+  //   dropdown: false,
+  //   wind: false,
+  //   temperature: false
+  // };
+  constructor(private router: Router,
+     private authService: AuthService,private elRef: ElementRef) {}
 
   ngOnInit() {}
   get isLoggedIn(): boolean {
     return this.authService.getIsLoggedIn();
   }
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    if (!this.elRef.nativeElement.contains(event.target)) {
+      // this.isDropdownOpen = false;
+    }
+  }
+
+  // toggleDropdown(dropdownName: string) {
+  //   // Close all dropdowns
+  //   for (let key in this.dropdownVisible) {
+  //     if (key !== dropdownName) {
+  //       this.dropdownVisible[key] = false;
+  //     }
+  //   }
+
+
+  //   this.dropdownVisible[dropdownName] = !this.dropdownVisible[dropdownName];
+  // }
+
+
+
+  toggleFormVisibility() {
+    this.isFormVisible = !this.isFormVisible;
+  }
   forecastPage() {
     this.router.navigate(['/landing-page']);
   }
+ 
 }
