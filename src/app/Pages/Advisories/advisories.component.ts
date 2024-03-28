f3f93fbb7ed9726a2faf11d4c33b3c306be81e02
sbongi-seni <109135904+sbongi-seni@users.noreply.github.com>
2024-03-28 08:23:32 +0200

    SAWS-235 back Update

--- src/app/Pages/Advisories/advisories.component.ts
+++ src/app/Pages/Advisories/advisories.component.ts
@@ -1,103 +1,121 @@
 import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
-import {APIService} from './../../services/apis.service'
+import { APIService } from './../../services/apis.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { LoadingController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
 
 export interface textFile {
-  foldername:string,
-  filename:string,
-  lastmodified:string,
-  filetextcontent:string,
+  foldername: string;
+  filename: string;
+  lastmodified: string;
+  filetextcontent: string;
 }
 @Component({
   selector: 'app-advisories',
   templateUrl: './advisories.component.html',
   styleUrls: ['./advisories.component.scss'],
 })
-export class AdvisoriesComponent  implements OnInit {
-  isLoading:boolean = true;
-  AdvisoriesList:any = [];
-  VolcanoList:any = [];
-  CycloneList:any = [];
-  swxcList:any = [];
-  filter:any;
-  filter2:any;
-  constructor(private api:APIService,
-    private spinner: NgxSpinnerService) { }
-    @HostListener('window:scroll', ['$event'])
-    onWindowScroll($event:any): void {
-      debugger;
-      this.scrollBuytton();
-    }
+export class AdvisoriesComponent implements OnInit {
+  isLoading: boolean = true;
+  AdvisoriesList: any = [];
+  VolcanoList: any = [];
+  CycloneList: any = [];
+  swxcList: any = [];
+  filter: any;
+  filter2: any;
+  constructor(
+    private api: APIService,
+    private spinner: NgxSpinnerService,
+    private router: Router
+  ) {}
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll($event: any): void {
+    debugger;
+    this.scrollBuytton();
+  }
 
   ngOnInit() {
-    var Volcanos:any = [];
-    var Cyclones:any = [];
-    var SWX:any = [];
-    this.api.GetSourceTextFolderFiles("alerts")
-    .subscribe((Response) => {
-      Response.forEach((element:any) => {
+    var Volcanos: any = [];
+    var Cyclones: any = [];
+    var SWX: any = [];
+    this.api.GetSourceTextFolderFiles('alerts').subscribe((Response) => {
+      Response.forEach((element: any) => {
         element.Id = element.filetextcontent.split('\n')[2];
-        if(element.Id.split(' ')[0] == 'VA'){
+        if (element.Id.split(' ')[0] == 'VA') {
           var vwValue = element.filetextcontent.split('\n')[5];
           element.heading = vwValue;
           vwValue = vwValue.split('VOLCANO: ')[1];
-          if(vwValue == undefined){
+          if (vwValue == undefined) {
             vwValue = element.filetextcontent.split('\n')[5];
           }
-          var obj = { value: element.filetextcontent.split('\n')[0], viewValue:vwValue}
+          var obj = {
+            value: element.filetextcontent.split('\n')[0],
+            viewValue: vwValue,
+          };
           this.VolcanoList.push(obj);
           element.Id = element.filetextcontent.split('\n')[0];
         }
-        if(element.Id.split(' ')[0] == 'TC'){
-          debugger
-          var vwValue = element.filetextcontent.split('\n')[5]
+        if (element.Id.split(' ')[0] == 'TC') {
+          debugger;
+          var vwValue = element.filetextcontent.split('\n')[5];
           element.heading = vwValue;
           vwValue = vwValue.split('TC:')[1].trim();
-          var obj = { value: element.filetextcontent.split('\n')[0], viewValue:vwValue}
+          var obj = {
+            value: element.filetextcontent.split('\n')[0],
+            viewValue: vwValue,
+          };
           this.CycloneList.push(obj);
           element.Id = element.filetextcontent.split('\n')[0];
         }
       });
       this.AdvisoriesList = Response;
-      console.log("Response ", this.AdvisoriesList);
-      debugger
+      console.log('Response ', this.AdvisoriesList);
+      debugger;
       this.isLoading = false;
       this.spinner.hide();
-    })
+    });
 
     document.addEventListener('scroll', () => {
-      debugger
-      var Topbutton = document.getElementById("btntotop");
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        Topbutton!.style.display = "block";
+      debugger;
+      var Topbutton = document.getElementById('btntotop');
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        Topbutton!.style.display = 'block';
       } else {
-        Topbutton!.style.display = "none";
+        Topbutton!.style.display = 'none';
       }
-    })
+    });
   }
 
-  ScrollToFilter(event:any){
-    debugger
+  ScrollToFilter(event: any) {
+    debugger;
     var element = document.getElementById(event.target.value);
-    element?.scrollIntoView({behavior: 'smooth'});
+    element?.scrollIntoView({ behavior: 'smooth' });
   }
 
-  ScrollToTop(value:any){
-    debugger
+  ScrollToTop(value: any) {
+    debugger;
     var element = document.getElementById(value);
-    element?.scrollIntoView({behavior: 'smooth'});
+    element?.scrollIntoView({ behavior: 'smooth' });
   }
 
-  scrollBuytton(){
-    debugger
+  scrollBuytton() {
+    debugger;
     let buttoTop = document.getElementById('btntotop');
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      buttoTop!.style.display = "block";
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      buttoTop!.style.display = 'block';
     } else {
-      buttoTop!.style.display = "none";
+      buttoTop!.style.display = 'none';
     }
   }
-  
 
+  forecastPage() {
+    window.history.back();
+    // this.router.navigate(['/forecast']);
+  }
 }
