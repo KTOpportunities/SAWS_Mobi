266bc37d0a79b4039567a3d26ab4822a6404944d
ceekem <37778925+ceekem@users.noreply.github.com>
2026-01-27 16:37:22 +0200

    payfast ios url fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@ export const environment = {
 
   production: true,
   //serverAPI: 'http://aviapp.weathersa.co.za:81/api/',
-   //serverAPI: 'https://cors-any-where-15198095e4b9.herokuapp.com/http://qa.j-cred.co.za/aviappapi/api/',
- serverAPI: 'http://qa.j-cred.co.za/aviappapi/api/',
+  serverAPI: 'https://aviationapp.weathersa.co.za/api/',
 
 };
